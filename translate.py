28208b72cc4790ba318c07b4d7f6383e54272d9c
Samuel Façanha <samufacanha@gmail.com>
2023-11-13 17:33:16 -0300

    feat: added recursive folder r/w

--- translate.py
+++ translate.py
@@ -27,7 +27,7 @@ def translate_text(text, model="gpt-3.5-turbo", target_language="es"):
             messages=[
                 {
                     "role": "user",
-                    "content": f"Translate the values of following dictionary(en) text to {target_language}: {text} \n don't translate the dictionary keys, nor what is inside the double curly braces. Don't write anything else than the translation.",
+                    "content": f"Translate the values of following dictionary(en) text to {target_language}: {text} \n don't translate the dictionary keys. do not translate what is inside the double curly braces {{ like this }}. Don't write anything else than the translation.",
                 },
             ],
         )
@@ -52,21 +52,24 @@ def translate_and_save_files(directory, target_languages, failed_files=None):
     if failed_files is None:
         failed_files = []
 
-        # Read and store the contents of each JSON file
-        for filename in os.listdir(directory):
-            if filename.endswith(".json"):
-                file_path = os.path.join(directory, filename)
-                if os.stat(file_path).st_size == 0:
-                    continue
+        # Recursively read and store the contents of each JSON file
+        for root, _, files in os.walk(directory):
+            for filename in files:
+                if filename.endswith(".json"):
+                    file_path = os.path.join(root, filename)
+                    if os.stat(file_path).st_size == 0:
+                        continue
 
-                with open(file_path, "r", encoding="utf-8") as file:
-                    all_contents[filename] = json.load(file)
+                    with open(file_path, "r", encoding="utf-8") as file:
+                        # Construct a unique identifier for the file
+                        relative_path = os.path.relpath(file_path, directory)
+                        all_contents[relative_path] = json.load(file)
     else:
         # Load only the contents of the failed files
-        for filename, _ in failed_files:
-            file_path = os.path.join(directory, filename)
+        for file_path, _ in failed_files:
+            file_path = os.path.join(directory, file_path.replace("_", os.sep))
             with open(file_path, "r", encoding="utf-8") as file:
-                all_contents[filename] = json.load(file)
+                all_contents[file_path] = json.load(file)
 
     new_failed_files = []
 
@@ -75,20 +78,37 @@ def translate_and_save_files(directory, target_languages, failed_files=None):
         if not os.path.exists(lang_directory):
             os.makedirs(lang_directory)
 
-        for filename, content in all_contents.items():
+        for file_path, content in all_contents.items():
+            original_file_path = file_path
+            current_directory = lang_directory
+            while "/" in file_path[2:]:
+                current_directory = os.path.join(
+                    current_directory, file_path[: file_path.find("/")]
+                )
+                if not os.path.exists(current_directory):
+                    os.makedirs(current_directory)
+                file_path = file_path[file_path.find("/") + 1 :]
+
             translated_content = translate_text(
                 json.dumps(content), target_language=lang
             )
 
             if translated_content is None or translated_content == "":
-                new_failed_files.append((filename, lang))
+                new_failed_files.append((file_path, lang))
                 continue
 
-            translated_file_path = os.path.join(lang_directory, filename)
+            translated_file_path = os.path.join(lang_directory, original_file_path)
             with open(translated_file_path, "w", encoding="utf-8") as file:
-                json.dump(
-                    json.loads(translated_content), file, ensure_ascii=False, indent=2
-                )
+                try:
+                    json.dump(
+                        json.loads(translated_content),
+                        file,
+                        ensure_ascii=False,
+                        indent=2,
+                    )
+                except:
+                    new_failed_files.append((file_path, lang))
+                    continue
 
             print(
                 f"File {bcolors.OKCYAN} {translated_file_path} {bcolors.ENDC} translated to {bcolors.OKGREEN} {lang} {bcolors.ENDC} successfully."
